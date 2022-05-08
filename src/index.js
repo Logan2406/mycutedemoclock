d8aa55741a1c82b5a23d0c7501a3da5b356411ea
Jyoti Pravo Pal <96343377+Logan2406@users.noreply.github.com>
2022-05-08 16:48:33 +0530

    This is second commint

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ export const MyCuteClock = () => {
   const [mon,setMon] = useState("");
   const [yr,setYr] = useState("");
   const [arr2,setArr2] = useState([]);
+  let somearr=[];
   
   
   useEffect(()=>
@@ -26,18 +27,35 @@ export const MyCuteClock = () => {
       {
           setDate(new Date().toString());
           setTimer(new Date().toLocaleTimeString());
-          setArr(timer.split(":"));
-          setArr2(date.split(" "));
-          setHour(arr[0]);
-          setMin(arr[1]);
-          setSec(arr[2].split(" ")[0]);
-          setAp(arr[2].split(" ")[1]);
 
-          setDay(arr2[0]);
-          setDat(arr2[2]);
-          setMon(arr2[1]);
-          setYr(arr2[3]);
-          console.log(arr);
+          if(timer!=undefined && timer!="")
+          {
+            setArr(timer.split(":"));
+            setHour(arr[0]);
+            setMin(arr[1]);
+            if(arr!=undefined && arr!=[])
+            {
+                if(arr[2]!=undefined && arr[2]!="")
+                {
+                    somearr = arr[2].split(" ");
+                    setSec(somearr[0]);
+                    setAp(somearr[1]);
+                }
+            }
+           
+            
+
+          }
+
+          if(date!=undefined && date!="")
+          {
+            setArr2(date.split(" "));
+            setDay(arr2[0]);
+            setDat(arr2[2]);
+            setMon(arr2[1]);
+            setYr(arr2[3]);
+          }
+         
       },1000)
       
       return () => clearInterval(time);
@@ -53,32 +71,18 @@ export const MyCuteClock = () => {
                     <div className={styles.min}><h4>{min}</h4></div>
                     <div className={styles.sep}><h4>{":"}</h4></div>
                     <div className={styles.sec}><h4>{sec}</h4></div>
-                    <div>
-                        
-                        <div className={styles.datediv}>
-                            <div>
-                                <h3 >{day}</h3>
-                            </div>
-                            <div >
-                                <h3>{dat} {mon}</h3>
-                            </div>
-                            <div> 
-                                <h3 >{yr}</h3>
-                            </div>
-                       </div>
-                       <div className={styles.ap}><h4>{ap}</h4></div>
-                       
-                        
+                    <div className={styles.ap}><h4>{ap}</h4></div>
+                </div>
+                <div className={styles.datedivcon}>
+                    <div className={styles.datediv}>
+                        <div><h3>{day}</h3></div>
+                        <div ><h3>{dat} {mon}</h3></div>
+                        <div> <h3 >{yr}</h3></div>
                     </div>
                     
-
-
-                        
-                        
-                      
-                   
                 </div>
+                
             </div>
-        </div>
+    </div>
   )
 }
